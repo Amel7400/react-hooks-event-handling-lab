b964e4b15ca26d5e4001cf3902ab5e0b98f64be7
AMEL BATOUCHE <amelbat7400@gmail.com>
2023-09-24 18:36:12 +0200

    React Event Assignement Completed

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,21 @@
-// Code Keypad Component Here
+import React from "react";
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+  // Event handler for the onChange event
+  const handleInputChange = (event) => {
+    console.log("Entering password...");
+  };
+
+  return (
+    <div>
+      <input
+        type="password"
+        name="password" // You can change this name if needed
+        onChange={handleInputChange} // Attach the event handler to the onChange event
+        value="Entering password..."
+      />
+    </div>
+  );
 }
 
 export default Keypad;
